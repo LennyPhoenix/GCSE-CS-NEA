b5c0483bd3f37f3dbcdf5d50083cde540fc12bf8
Lenny Critchley <LennyPhoenixC@gmail.com>
2021-09-24 22:50:53 +0100

    :sparkles: Add pymunk space to GameManager

--- src/game_manager.py
+++ src/game_manager.py
@@ -6,9 +6,28 @@ Classes:
     GameManager
 """
 
+import pyglet
+import pymunk
+
 
 class GameManager:
     """
     The game manager, controls all game processes - such as physics - and
     contains the dungeon itself along with any entities.
     """
+
+    # The time between each fixed update function call
+    FIXED_UPDATE_TIMESTEP = 1/60
+
+    def __init__(self):
+        self.physics_space = pymunk.Space()
+
+        # Set up our physics update method
+        pyglet.clock.schedule_interval(self.on_fixed_update,
+                                       self.FIXED_UPDATE_TIMESTEP)
+
+    def on_fixed_update(self, dt: float):
+        self.physics_space.step(self.FIXED_UPDATE_TIMESTEP)
+
+    def __del__(self):
+        pyglet.clock.unschedule(self.on_fixed_update)
