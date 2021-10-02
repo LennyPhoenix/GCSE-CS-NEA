522cc5630dc07aebeae28da358e6230a0f674f4d
Lenny Critchley <LennyPhoenixC@gmail.com>
2021-10-03 00:22:05 +0100

    :recycle: Refactor AABB to use Object2D

--- src/aabb.py
+++ src/aabb.py
@@ -12,43 +12,35 @@ Classes:
 
 from __future__ import annotations  # NOTE: This is necessary below Python 3.10
 
+from .object2d import Object2D
+
 import pyglet
 
-from typing import Tuple
+from typing import Optional, Tuple
 
 
-class AABB:
+class AABB(Object2D):
     # TODO: Add get_broad_phase method
-    # TODO: Figure out better way of doing offsets
 
     debug_rect: pyglet.shapes.Rectangle = None
 
-    def __init__(self,
-                 x: float,
-                 y: float,
-                 w: float,
-                 h: float,
-                 offset: Tuple[float, float] = (0, 0),
-                 layer: int = 0xFFFFFFFF,
-                 mask: int = 0xFFFFFFFF):
+    def __init__(
+        self,
+        x: float,
+        y: float,
+        w: float,
+        h: float,
+        layer: int = 0xFFFFFFFF,
+        mask: int = 0xFFFFFFFF,
+        parent: Optional[Object2D] = None
+    ):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
-        self.offset = offset
         self.layer = layer
         self.mask = mask
-
-    def __repr__(self) -> str:
-        return f"AABB({self.x}, {self.y}, {self.w}, {self.h})"
-
-    @property
-    def position(self) -> Tuple[float, float]:
-        return (self.x, self.y)
-
-    @position.setter
-    def position(self, new_position: Tuple[float, float]):
-        self.x, self.y = new_position
+        self.parent = parent
 
     @property
     def extends(self) -> Tuple[float, float]:
@@ -60,18 +52,18 @@ class AABB:
 
     def is_colliding_aabb(self, other: AABB) -> bool:
         return (
-            self.x + self.offset[0] <= other.x + other.offset[0] + other.w
-            and self.x + self.offset[0] + self.w >= other.x + other.offset[0]
-            and self.y + self.offset[1] <= other.y + other.offset[1] + other.h
-            and self.y + self.offset[1] + self.h >= other.y + other.offset[1]
+            self.global_x <= other.global_x + other.w
+            and self.global_x + self.w >= other.global_x
+            and self.global_y <= other.global_y + other.h
+            and self.global_y + self.h >= other.global_y
         )
 
     def is_colliding_point(self, point: Tuple[float, float]) -> bool:
         return (
-            self.x + self.offset[0] <= point[0]
-            and point[0] <= self.x + self.offset[0] + self.w
-            and self.y + self.offset[1] <= point[1]
-            and point[1] <= self.y + self.offset[1] + self.h
+            self.global_x <= point[0]
+            and point[0] <= self.global_x + self.w
+            and self.global_y <= point[1]
+            and point[1] <= self.global_y + self.h
         )
 
     def create_debug_rect(
@@ -81,26 +73,20 @@ class AABB:
         group: pyglet.graphics.Group = None
     ):
         self.debug_rect = pyglet.shapes.Rectangle(
-            x=self.x,
-            y=self.y,
+            x=self.global_x,
+            y=self.global_y,
             width=self.w,
             height=self.h,
             color=colour,
             batch=batch,
             group=group
         )
-        self.debug_rect.anchor_position = (-self.offset[0], -self.offset[1])
 
-    def update_debug_rect(
-        self,
-        offset: Tuple[float, float] = (0, 0)
-    ):
+    def update_debug_rect(self):
         # NOTE: By calling _update_position manually we only need to update
         #       vertex coordinates once.
-        self.debug_rect._x, self.debug_rect._y = self.position
+        self.debug_rect._x, self.debug_rect._y = self.global_position
         self.debug_rect._width, self.debug_rect._height = self.extends
-        self.debug_rect._anchor_x = -self.offset[0]
-        self.debug_rect._anchor_y = -self.offset[1]
         self.debug_rect._update_position()
 
     def __del__(self):
